56db238eb52f7240b200dee940de7e8cfdb642ac
pramod pawar <kingpramodpawar511@gmail.com>
2024-04-05 20:27:40 +0530

    created an filter funcationality

--- src/features/product-list/productSlice.js
+++ src/features/product-list/productSlice.js
@@ -1,30 +1,53 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProducts } from "./productApis";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { fetchAllProducts,fetchProductsByFilters } from './productApis';
 
 const initialState = {
   products: [],
-  status: "idle",
+  status: 'idle',
 };
 
+
 export const fetchAllProductsAsync = createAsyncThunk(
-  "product/fetchAllProducts",
+  'product/fetchAllProducts',
   async () => {
     const response = await fetchAllProducts();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilters(filter,sort);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+
 
 export const productSlice = createSlice({
-  name: "product",
+  name: 'product',
   initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchAllProductsAsync.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        state.status = "idle";
+        state.status = 'idle';
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
         state.products = action.payload;
       });
   },
